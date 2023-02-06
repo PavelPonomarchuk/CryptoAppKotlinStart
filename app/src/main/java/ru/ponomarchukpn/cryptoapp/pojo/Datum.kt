d85fb69c8bd33b83313575e2fef8d94d21195e99
PavelPonomarchuk <ponomarchuk83@gmail.com>
2023-02-06 13:34:30 +0500

    Добавлены классы pojo

---
+++ app/src/main/java/ru/ponomarchukpn/cryptoapp/pojo/Datum.kt
@@ -0,0 +1,10 @@
+package ru.ponomarchukpn.cryptoapp.pojo
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class Datum(
+    @SerializedName("CoinInfo")
+    @Expose
+    val coinInf: CoinInfo? = null
+)
