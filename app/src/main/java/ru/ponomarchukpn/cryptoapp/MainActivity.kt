924fd5d325efcd624e4ad219b78c5e03f32e623e
PavelPonomarchuk <ponomarchuk83@gmail.com>
2023-02-06 15:05:16 +0500

    Добавлена загрузка данных от api

--- app/src/main/java/ru/ponomarchukpn/cryptoapp/MainActivity.kt
+++ app/src/main/java/ru/ponomarchukpn/cryptoapp/MainActivity.kt
@@ -2,10 +2,40 @@ package ru.ponomarchukpn.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import ru.ponomarchukpn.cryptoapp.api.ApiFactory
 
 class MainActivity : AppCompatActivity() {
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val disposable = ApiFactory.apiService.getFullPriceList(fSyms = "BTC,ETH,EOS")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("TEST_OF_LOADING_DATA", it.toString())
+            }, {
+                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+            })
+//        val disposable = ApiFactory.apiService.getTopCoinsInfo()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({
+//                Log.d("TEST_OF_LOADING_DATA", it.toString())
+//            }, {
+//                it.message?.let { it1 -> Log.d("TEST_OF_LOADING_DATA", it1) }
+//            })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
     }
 }
